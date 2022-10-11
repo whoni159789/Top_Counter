d46055d4fb44596f50f8cd0f230ff7ee06adc168
whoni159789 <whoni6131@gmail.com>
2022-10-11 19:45:33 +0900

    README 수정

--- sim_1/new/tb_Counter.v
+++ sim_1/new/tb_Counter.v
@@ -2,29 +2,19 @@
 
 module tb_Counter();
 
-    reg i_btn;
+    reg i_clk;
     reg i_reset;
-    wire [2:0] o_led;
+    wire [7:0] o_led;
 
     Top_Counter dut(
-        .i_btn(i_btn),
+        .i_clk(i_clk),
         .i_reset(i_reset),
         .o_led(o_led)
     );
 
     initial begin
         #00 i_btn = 1'b0; i_reset = 1'b0;
-        for(integer i=0; i<10; i=i+1)
-        begin
-            #10 i_btn = 1'b1; i_reset = 1'b0;
-            #10 i_btn = 1'b0; i_reset = 1'b0;
-        end
-        #10 i_btn = 1'b0; i_reset = 1'b1;
-        for(integer i=0; i<10; i=i+1)
-        begin
-            #10 i_btn = 1'b1; i_reset = 1'b0;
-            #10 i_btn = 1'b0; i_reset = 1'b0;
-        end
+        
         #10 $finish;
     end
 endmodule
